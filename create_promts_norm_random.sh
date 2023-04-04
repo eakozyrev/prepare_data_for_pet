1980b40c1f053f384a8e6eb2abca2dec8ffefb83
eakozyrev <51480988+eakozyrev@users.noreply.github.com>
2023-04-04 13:34:22 -0400

    Update create_promts_norm_random.sh

--- create_promts_norm_random.sh
+++ create_promts_norm_random.sh
@@ -1,15 +1,18 @@
+#!/usr/bin/env bash
+
 aread=1 #initial number to read
 awrite=1  #initial number to write
 pathwrite=promts_norm_random
+mkdir $pathwrite
 
 while [ $aread -lt 3 ]
 do
     echo $aread
-    promt=../anon/r00$aread\_WB_FDG_6R7/HImg/r00$aread\_WB_FDG_6R7_50m-60m_20x9_4mm_p.view.img
+    promt=$1/r00$aread\_WB_FDG_6R7/HImg/r00$aread\_WB_FDG_6R7_50m-60m_20x9_4mm_p.view.img
     ls $promt
-    rand=../anon/r00$aread\_WB_FDG_6R7/HImg/r00$aread\_WB_FDG_6R7_50m-60m_20x9_4mm_3_final.randview.img
+    rand=$1/r00$aread\_WB_FDG_6R7/HImg/r00$aread\_WB_FDG_6R7_50m-60m_20x9_4mm_3_final.randview.img
     ls $rand
-    norm=../anon/r00$aread\_WB_FDG_6R7/HImg/dircor_WB_FDG_6R7_20x9_4mm.0dgns_view.img
+    norm=$1/r00$aread\_WB_FDG_6R7/HImg/dircor_WB_FDG_6R7_20x9_4mm.0dgns_view.img
     ls $norm
     imagio_sum -scale 1 $promt -1 $rand tmp.img
     imagio_prod -scale 1000 -exponent 1.0 tmp.img -1.0 $norm $pathwrite/promt_$awrite\.img
