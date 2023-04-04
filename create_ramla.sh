683f84db062d79627e6df34173c89815fdf3b70f
eakozyrev <51480988+eakozyrev@users.noreply.github.com>
2023-04-04 12:39:47 -0400

    Update create_ramla.sh

--- create_ramla.sh
+++ create_ramla.sh
@@ -1,11 +1,16 @@
+#!/usr/bin/env bash
+
 aread=1 #initial number to read
 awrite=1  #initial number to write
 pathwrite=ramla
+echo $1 # where reconstructed patients are placed
 
 while [ $aread -lt 3 ]
 do
     echo $aread
-    ramla=../anon/r00$aread\_WB_FDG_6R7/Img/r00$aread\_WB_FDG_6R7_50m-60m_dir-rml_4mm_RM_c1.img
+    ramla=$1/r00$aread\_WB_FDG_6R7/Img/r00$aread\_WB_FDG_6R7_50m-60m_dir-rml_4mm_RM_c1.img
+    echo $ramla
+    mkdir $pathwrite
     petview2interfile $ramla -floatii -ofo -of $pathwrite/ramla_$awrite\.i
     python plot.py $pathwrite/ramla_$awrite\.i $pathwrite/ramla_$awrite\.i.png
     awrite=`expr $awrite + 1`
